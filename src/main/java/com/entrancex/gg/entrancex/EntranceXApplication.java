7dbbb01eecd85b17df95ef2523d58c5633238404
XtraYuki <aungkhentzaw@gmail.com>
2023-11-14 23:36:12 +0630

    initial

---
+++ src/main/java/com/entrancex/gg/entrancex/EntranceXApplication.java
@@ -0,0 +1,13 @@
+package com.entrancex.gg.entrancex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntranceXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntranceXApplication.class, args);
+    }
+
+}
